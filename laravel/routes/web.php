8a6964214fe2b37b55437542524dcfcc07d4d3b1
rogozhuk <resmedia@ya.ru>
2020-03-07 13:28:10 +0300

    #edit user settings

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,8 +18,9 @@ Route::group(
         'prefix' => 'admin',
         'as' => 'admin.'
     ], function () {
+    Route::match(['post', 'get'], '/account', 'Admin\AccountController@update')->name('account');
     Route::get('/', 'Admin\IndexController@index');
-    Route::resource('/news', 'Admin\NewsController')->only('index');
+    Route::resource('/news', 'Admin\NewsController');
     Route::resource('/news/store', 'Admin\NewsController');
     Route::resource('/news/update', 'Admin\NewsController')->only('update');
     Route::get('/news/delete/{id}', 'Admin\NewsController@deleteItem');
